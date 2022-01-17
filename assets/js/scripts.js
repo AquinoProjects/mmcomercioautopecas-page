645489f048e33756f454499b45a67fd945bf9995
“Daniel <daniel@grupovex.com.br>
2022-01-17 15:55:40 -0300

    Chamada do método getFullYear no footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,26 +7,30 @@ function scrollFunctionButton() {
     if (window.pageYOffset > 200) {
         setTimeout(function () {
             backToTopButton.style.display = "flex";
-         }, 1000);
+        }, 1000);
     }
-    else { 
+    else {
         setTimeout(function () {
-         backToTopButton.style.display = "none";
-      }, 1000);
-        
+            backToTopButton.style.display = "none";
+        }, 1000);
+
     }
 }
 
 /*jQuery para atualizar a home ao enviar o formulário*/
-$(function() {
-    $('.form-grid').bind('submit', function(){ 
+$(function () {
+    $('.form-grid').bind('submit', function () {
         setTimeout(function () {
             window.location.reload(1);
         }, 5000);
     });
 });
 
+//Jogar ano atual para o footer
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
+
+//Carousel banners
 let carousel = document.querySelector('.carousel');
 
 let carouselInner = document.querySelector('.carousel-inner');
@@ -35,7 +39,7 @@ let prev = document.querySelector('.carousel-controls .prev');
 
 let next = document.querySelector('.carousel-controls .next');
 
-let slides =  document.querySelectorAll('.carousel-inner .carousel-item');
+let slides = document.querySelectorAll('.carousel-inner .carousel-item');
 
 let totalSlides = slides.length;
 
@@ -63,32 +67,32 @@ loop(true);
 
 //Carousel events
 
-next.addEventListener('click',()=>{
+next.addEventListener('click', () => {
     slideToNext();
 });
 
-prev.addEventListener('click',()=>{
+prev.addEventListener('click', () => {
     slideToPrev();
 });
 
-carouselInner.addEventListener('transitionend',()=>{
-    if(direction === -1){
-        if(jump > 1){
-            for(let i = 0; i < jump; i++){
+carouselInner.addEventListener('transitionend', () => {
+    if (direction === -1) {
+        if (jump > 1) {
+            for (let i = 0; i < jump; i++) {
                 activeSlide++;
                 carouselInner.append(carouselInner.firstElementChild);
             }
-        }else{
+        } else {
             activeSlide++;
             carouselInner.append(carouselInner.firstElementChild);
         }
-    }else if(direction === 1){
-        if(jump > 1){
-            for(let i = 0; i < jump; i++){
+    } else if (direction === 1) {
+        if (jump > 1) {
+            for (let i = 0; i < jump; i++) {
                 activeSlide--;
                 carouselInner.prepend(carouselInner.lastElementChild);
             }
-        }else{
+        } else {
             activeSlide--;
             carouselInner.prepend(carouselInner.lastElementChild);
         }
@@ -96,88 +100,88 @@ carouselInner.addEventListener('transitionend',()=>{
 
     carouselInner.style.transition = 'none';
     carouselInner.style.transform = 'translateX(0%)';
-    setTimeout(()=>{
+    setTimeout(() => {
         jump = 1;
         carouselInner.style.transition = 'all ease .5s';
     });
     updateIndicators();
 });
 
-document.querySelectorAll('.carousel-indicators span').forEach(item=>{
-    item.addEventListener('click',(e)=>{
+document.querySelectorAll('.carousel-indicators span').forEach(item => {
+    item.addEventListener('click', (e) => {
         let slideTo = parseInt(e.target.dataset.slideTo);
-        
+
         let indicators = document.querySelectorAll('.carousel-indicators span');
 
-        indicators.forEach((item,index)=>{
-            if(item.classList.contains('active')){
+        indicators.forEach((item, index) => {
+            if (item.classList.contains('active')) {
                 activeIndicator = index
             }
         })
 
-        if(slideTo - activeIndicator > 1){
+        if (slideTo - activeIndicator > 1) {
             jump = slideTo - activeIndicator;
             step = jump * step;
             slideToNext();
-        }else if(slideTo - activeIndicator === 1){
+        } else if (slideTo - activeIndicator === 1) {
             slideToNext();
-        }else if(slideTo - activeIndicator < 0){
+        } else if (slideTo - activeIndicator < 0) {
 
-            if(Math.abs(slideTo - activeIndicator) > 1){
+            if (Math.abs(slideTo - activeIndicator) > 1) {
                 jump = Math.abs(slideTo - activeIndicator);
                 step = jump * step;
                 slideToPrev();
             }
-                slideToPrev();
+            slideToPrev();
         }
-        step = 100 / totalSlides; 
+        step = 100 / totalSlides;
     })
 });
 
-carousel.addEventListener('mouseover',()=>{
+carousel.addEventListener('mouseover', () => {
     loop(false);
 })
 
-carousel.addEventListener('mouseout',()=>{
+carousel.addEventListener('mouseout', () => {
     loop(true);
 })
 
 //Carousel functions
 
-function loadIndicators(){
-    slides.forEach((slide,index)=>{
-        if(index === 0){
+function loadIndicators() {
+    slides.forEach((slide, index) => {
+        if (index === 0) {
             document.querySelector('.carousel-indicators').innerHTML +=
-            `<span data-slide-to="${index}" class="active"></span>`;
-        }else{
+                `<span data-slide-to="${index}" class="active"></span>`;
+        } else {
             document.querySelector('.carousel-indicators').innerHTML +=
-            `<span data-slide-to="${index}"></span>`;
+                `<span data-slide-to="${index}"></span>`;
         }
-    }); 
+    });
 };
 
-function updateIndicators(){
-    if(activeSlide > (totalSlides - 1)){
+function updateIndicators() {
+    if (activeSlide > (totalSlides - 1)) {
         activeSlide = 0;
-    }else if(activeSlide < 0){
+    } else if (activeSlide < 0) {
         activeSlide = (totalSlides - 1);
     }
     document.querySelector('.carousel-indicators span.active').classList.remove('active');
     document.querySelectorAll('.carousel-indicators span')[activeSlide].classList.add('active');
 };
 
-function slideToNext(){
-    if(direction === 1){
+function slideToNext() {
+    if (direction === 1) {
         direction = -1;
         carouselInner.prepend(carouselInner.lastElementChild);
     };
-    
+
     carousel.style.justifyContent = 'flex-start';
     carouselInner.style.transform = `translateX(-${step}%)`;
 };
 
-function slideToPrev(){
-    if(direction === -1){
+function slideToPrev() {
+    if (direction === -1) {
         direction = 1;
         carouselInner.append(carouselInner.firstElementChild);
     };
@@ -185,12 +189,12 @@ function slideToPrev(){
     carouselInner.style.transform = `translateX(${step}%)`;
 };
 
-function loop(status){
-    if(status === true){
-        time = setInterval(()=>{
+function loop(status) {
+    if (status === true) {
+        time = setInterval(() => {
             slideToNext();
-        },interval);
-    }else{
+        }, interval);
+    } else {
         clearInterval(time);
     }
 }
